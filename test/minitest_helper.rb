7e0d6ec251d968d4dcd1d5956acf993e37c79db8
SHIBATA Hiroshi <hsbt@ruby-lang.org>
2015-05-18 10:23:58 +0900

    dependency

--- test/minitest_helper.rb
+++ test/minitest_helper.rb
@@ -1,4 +1,7 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+
+require 'rake'
 require 'delayed_job_slack_notification'
+load 'tasks/delayed_job_slack_notification.rake'
 
 require 'minitest/autorun'
